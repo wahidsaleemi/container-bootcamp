0c6afd78c3fc7da799fcd3992956d5098d333273
desreela <40350122+desreela@users.noreply.github.com>
2018-06-24 17:37:40 +0530

    Update Footer.vue

--- app/web/src/components/Footer.vue
+++ app/web/src/components/Footer.vue
@@ -14,7 +14,7 @@
       <div class="col-lg-6">
       </div>
       <div class="col-lg-12 credits">
-        Azure Global BlueBelt Team 
+        Azure Global Blue Belt Team 
       </div>
       <div class="col-lg-6">
       </div>
