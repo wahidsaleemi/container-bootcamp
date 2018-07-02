9879046ac5be1c0942ae452783d2f99cb13e7943
desreela <40350122+desreela@users.noreply.github.com>
2018-07-03 00:08:12 +0530

    Update Footer.vue

--- app/web/src/components/Footer.vue
+++ app/web/src/components/Footer.vue
@@ -14,7 +14,7 @@ TEST  - Test<template>
       <div class="col-lg-6">
       </div>
       <div class="col-lg-12 credits">
-        Azure Global Belt Team Change2dw
+        Azure Global Belt Team Changedddddddddd
       </div>
       <div class="col-lg-6">
       </div>
